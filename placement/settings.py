efd02e1bc33ddf1829f087fd4be1e3e8b153df73
Harshil Mehta <62173089+mharshil009@users.noreply.github.com>
2024-01-04 18:23:09 +0530

    Update settings.py

--- placement/settings.py
+++ placement/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME' : 'placementdb',
         'USER' : 'postgres',
-        'PASSWORD' : '6989',
+        'PASSWORD' : 'new_password',
         'HOST' : '127.0.0.1',
     }
 }   
